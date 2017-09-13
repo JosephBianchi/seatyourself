0c9d4e0462ef619ca8505fbf9eed93edbface438
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-13 15:38:48 -0400

    temp put aside matts branch

--- seatyourself-Matt_branch/db/migrate/20170912183834_change_column_in_rest_to_string.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnInRestToString < ActiveRecord::Migration[5.0]
-  def change
-    change_column :restaurants, :tele, :string
-  end
-end
--- seatyourself-Matt_branch/db/migrate/20170912210836_changedaydatatype.rb
+++
@@ -1,6 +0,0 @@
-class Changedaydatatype < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :reservations, :day,:string
-    add_column :reservations, :day,:date
-  end
-end
