57df917d9f64db9d16acf22fbc2cc6fa54b6cdcd
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-14 17:16:25 -0400

    added validation tweak

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,8 +9,10 @@ class Reservation < ApplicationRecord
 
 
   def can_make
-    unless restaurant.has_capacity?(partysize, day, time)
-      errors.add(:base, "at max capacity for hour")
+    unless partysize == nil || time == nil
+      unless restaurant.has_capacity?(partysize, day, time)
+        errors.add(:base, "at max capacity for hour")
+      end
     end
   end
 
